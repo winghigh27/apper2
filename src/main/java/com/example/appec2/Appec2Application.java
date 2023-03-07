58366c6d3c70864504ba569dda60d486e735ab11
winghigh27 <winghigh27@gmail.com>
2023-03-07 10:41:17 +0900

    appec2

---
+++ src/main/java/com/example/appec2/Appec2Application.java
@@ -0,0 +1,13 @@
+package com.example.appec2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Appec2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Appec2Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/appec2/Appec2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.appec2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Appec2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
